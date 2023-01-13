1d3690cf24bd7c425d1eb48265b0dfba0facf896
kestin1780 <kestinhuahua@gmail.com>
2023-01-13 12:45:30 +0800

    [v2.2] 修整代码，使用ThreadPoolFactory创建Socket版本的线程池

--- rpc-core/src/main/java/com/test/socket/server/SocketServer.java
+++ rpc-core/src/main/java/com/test/socket/server/SocketServer.java
@@ -6,6 +6,7 @@ import com.test.registry.ServiceRegistry;
 import com.test.rpc.RpcServer;
 import com.test.rpc.RequestHandler;
 import com.test.serializer.CommonSerializer;
+import com.test.util.ThreadPoolFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,10 +22,6 @@ public class SocketServer implements RpcServer {
 
     private static final Logger logger = LoggerFactory.getLogger(SocketServer.class);
 
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 50;
-    private static final int KEEP_ALIVE_TIME = 60;
-    private static final int BLOCKING_QUEUE_CAPACITY = 100;
     private final ExecutorService threadPool;
     private RequestHandler requestHandler = new RequestHandler();
     private final ServiceRegistry serviceRegistry;
@@ -32,9 +29,7 @@ public class SocketServer implements RpcServer {
 
     public SocketServer(ServiceRegistry serviceRegistry) {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("socket-rpc-server");
     }
 
     @Override
